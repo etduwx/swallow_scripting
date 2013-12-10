b7378fd3e5e56875034e3ba5ec39a0b9ac0d9ed8
Edward Ma <edwardma@Edwards-MacBook-Pro.local>
2013-12-09 21:57:37 -0500

    Finished initial implementation of script with prims only, untested

--- src-back/run.py
+++ src-back/run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
 import sys, getopt 
-from subprocess import Popen, PIPE
+import signal
+import os
+import subprocess
 
 def main():
 
-	p1 = Popen(["tftp", "192.168.128.3"], stdout=PIPE, stdin=PIPE)
+	p1 = Popen(["tftp", "192.168.128.3"], stdout=PIPE, stdin=PIPE, shell=True,preexec_fn=os.setsid)
 	tftpargs = "mode binary\nput " + sys.argv[1] + ".sgb"
 	tftpargs = ''.join(tftpargs) 
 	output = p1.communicate(tftpargs)
+
+	sleep(2)
+
+	os.killpg(p1.pid,signal.SIGTERM)
 
 if __name__ == "__main__":
         main()
--- src-back/script.py
+++ src-back/script.py
@@ -1,5 +1,5 @@
 #!/bin/sh 
-import os, tftpy, subprocess
+import os, tftpy, subprocess, signal
 from time import sleep
 from subprocess import Popen, PIPE
 from tempfile import mkstemp
@@ -11,10 +11,16 @@ filename_xc = "blur.xc"
 match = "#define STAGES_OF_PIPELINE"
 
 outputfile = "output2.txt"
+outputreadfile = "print_output.txt"
 
 PARENTCORE_ID = 1
-NUMCORES_INIT
+NUMCORES_INIT = 36
+PRIMS_BODY_BASE_PATH = "Swallow-prim_base.xc"
+PRIMS_HEADER_BASE_PATH = "Swallow-prim_base.h"
+PRIMS_BODY_PATH = "Swallow-prim.xc"
+PRIMS_HEADER_PATH = "Swallow-prim.h"
 
+MCMAIN_PATH = "mcmain_prim.xc"
 
 def editFile(x):
 
@@ -47,6 +53,75 @@ def editFile(x):
 #		else:
 #			f.write(line)
 #	f.close() 	
+
+def getReplacementsPrims(typeofEditing):
+		return {
+			'power' : {'//control_channel':'control_channel', '//printMany(8':'printMany(8'},
+			'ratio' : {'//if(rank==0) printMany':'if(rank==0) printMany','//printer[0] = 1000*','printer[0] = 1000*'},
+			'time'  : {'//if(rank==0) printMany':'if(rank==0) printMany','//printer[0] = printer[1]','printer[0] = printer[1]'}
+			}.get(typeofEditing,{'//control_channel':'control_channel', '//printMany(8':'printMany(8'})
+
+def editPrimBody(typeOfEditing):
+	if (typeofEditing != "none"):
+		replacements = getReplacementsPrims(typeofEditing)
+		
+		fh, abs_path = mkstemp()
+		newFile = open(abs_path,'w')
+		editFile = open(PRIMS_BODY_BASEPATH)
+		for line in editFile:
+			for src,target in replacements.iteritems():
+				line = (line.replace(src,target))
+			new_file.write(line)
+		newFile.close()
+		close(fh)
+		editFile.close()
+		remove(PRIMS_BODY_PATH)
+		move(abs_path,PRIMS_BODY_PATH)
+	else:
+		remove(PRIMS_BODY_PATH)
+		copyfile(PRIMS_BODY_BASE_PATH,PRIMS_BODY_PATH)
+
+def editPrimHeader(coreList,measurePower):
+	numChildren = len(coreList)
+
+	match_x = "DIV_DEG_SWALLOW_X"
+	match_y = "DIV_DEG_SWALLOW_Y"
+
+	if(numChildren==4):
+		div_x = 2
+		div_y = 2
+	elif(numChildren==6):
+		div_x = 3
+		div_y = 2
+	elif(numChildren==8):
+		div_x=4
+		div_y=2
+	elif(numChildren==9):
+		div_x=3
+		div_y=3
+	elif(numChildren==12):
+		div_x=4
+		div_y=3
+	else:
+		div_x=4
+		div_y=4
+
+	basefile = open(PRIMS_HEADER_BASE_PATH)
+	fh, abs_path = mkstemp()
+	newFile = open(abs_path,'w')
+	for line in basefile:
+		if match_x in line:
+			newFile.write("#define DIV_DEG_SWALLOW_X" + str(div_x))
+		elif match_y in line:
+			newFile.write("#define DIV_DEG_SWALLOW_Y" + str(div_y))
+		else:
+			newFile.write(line)
+
+	newFile.close()
+	close(fh)
+	baseFile.close()
+	remove(PRIMS_HEADER_PATH)
+	move(abs_path,PRIMS_HEADER_PATH)
 
 def print_to_csv(appList,coreList,values):
 	f = open(outputfile,"a+")
@@ -88,80 +163,182 @@ def print_to_csv(appList,coreList,values):
 	f.close()		
 
 
-def editMCMain(appToAdd):
 
+# Need to add power or not power functionality
+def editMCMain(appToAdd,measurePower):
+	match = "par (int i = " + str(PARENTCORE_ID) 
+	fh, abs_path = mkstemp()
+	newFile = open(abs_path,'w')
+	baseFile = open(MCMAIN_BASE_PATH)
 
+	flag = 0
 
-def addPrim(coreList):
-	editMCMain("prim")
+	for line in baseFile:
+		if match in line:
+			flag = 1
+			newFile.write(line) 
+		elif flag  == 1:
+			newFile.write('on stdcore[i] : ' + str(appToAdd) + '_main(c[NCORES],1,k) ;')
+			newFile.write(line)
+			flag = 0
+		else:
+			newFile.write(line)
 
+	close(fh)
+	newFile.close()
+	baseFile.close()
+
+	remove(MCMAIN_PATH)
+	move(abs_path,MCMAIN_PATH)
+
+	MCMAIN_BASE_PATH = "mcmain_prim.xc"
+
+def addPrim(coreList, measurementType):
+	if measurementType == "power":
+		measurePower = True
+	else:
+		measurePower = False
+	editMCMain("prim",measurePower) #(isMethodRunningPowerMeasurements) ? True : False
+	editPrimHeader(coreList,measurePower)
+	editPrimBody(measurementType) # power=1,ratio=2,time=3
 
 def runExperiments(appList,coreNums):
-
+	
 	indices = [0] * len(appList)
+	states = ["none","none","none","none"]
 	appinds = [0] * 3
 	upperBound = 3 + sum(coreNums)
 	lastInd = 0
-	if any("prim" in s for s in appList):
-		coreList = range(4,4+coreNums)
-		addPrim(coreList)
+	cores = [0] * 3
+
 	if any("blur" in s for s in appList):
 		appinds[0] = curLastind
 		lastInd += 1
+		cores[curLastind] = coreNums[appList.index("blur")]
 	if any("sobel" in s for s in appList):
 		appinds[1] = curLastind
 		lastInd += 1
-	if any("sobel" in s for s in appList):
+		cores[curLastind] = coreNums[appList.index("sobel")]
+	if any("mergesort" in s for s in appList):
 		appinds[2] = curLastind
+		cores[curLastind] = coreNums[appList.index("mergesort")]
 		lastInd += 1
 
-	currentInd = lastInd
+	currentInd = lastInd - 1
 
-	while(--currentInd >= 0):
-		if any("blur" in s for s in appList):
-			coreList = range(4+indices[appinds[0]],4+indices[appinds[0]]+coreNums[appList.index("blur")])
-			addBlur(coreList)
+	if lastInd >=3:
+		lim3 = upperBound - cores[2]
+	else:
+		lim3 = 1
 
-		if any("sobel" in s for s in appList):
-			coreList = range(4+indices[appinds[1]],4+indices[appinds[1]]+coreNums[appList.index("sobel")])
-			addSobel(coreList)
+	if lastInd >=2:
+		lim2 = upperBound - cores[1]
+	else:
+		lim2 = 1
 
-		if any("mergesort" in s for s in appList):
-			coreList = range(4+indices[appinds[2]]4+indices[appinds[2]]+coreNums[appList.index("mergesort")])
-			addMergeSort(coreList)
+	if lastInd >=1:
+		lim1 = upperBound - cores[0]
+	else:
+		lim1 = 1
 
-		if (indices[currentInd] != upperBound):
-			indices[currentInd] += 1
-			break
-		else:
-			indices[currentInd] = 0
+	for i in xrange(lim1):
+		indices[0] = i
+		for j in xrange(lim2):
+			indices[1] = j
+			for k in xrange(lim3):
+				indices[2] = k
+				apps = []
+				values = []
+				coreList = []
+				if any("prim" in s for s in appList):
+					coreList.append(str(range(4,4+coreNums)))
+					apps.append("prim")
+				if any("blur" in s for s in appList):
+					coreList.append(str(range(4+indices[appinds[0]],4+indices[appinds[0]]+coreNums[appList.index("blur")])))
+					apps.append("blur")
+				if any("sobel" in s for s in appList):
+					coreList.append(str(range(4+indices[appinds[1]],4+indices[appinds[1]]+coreNums[appList.index("sobel")])))
+					apps.append("sobel")
+				if any("mergesort" in s for s in appList):
+					coreList.append(str(range(4+indices[appinds[2]]4+indices[appinds[2]]+coreNums[appList.index("mergesort")])))
+					apps.append("mergesort")
+				for coreAppIndex in xrange(len(appList)):
+					i = 0
+					while(i<4):
 
+						MCMAIN_BASE_PATH = "mcmain_prim_base.xc"
+						if (states[coreAppIndex] == "none" :
+							if coreAppIndex == 0:
+								states[coreAppIndex] = "power"
+							else:
+								i += 1
+								break
+						elif states[coreAppIndex] == "power":
+							states[coreAppIndex] = "ratio"
+						elif states[coreAppIndex] == "ratio":
+							states[coreAppIndex] = "time"
+						else:
+							states[coreAppIndex] = "none"
+							break
+
+						if any("prim" in s for s in appList):
+							addPrim(coreList[0],states[0])
+						if any("blur" in s for s in appList):
+							addBlur(coreList[appinds[0]],states[appinds[0]])
+						if any("sobel" in s for s in appList):
+							addSobel(coreList[appinds[1]],states[appinds[1]])
+						if any("mergesort" in s for s in appList):
+							addMergeSort(coreList[appinds[2]],states[appinds[2]])
+
+						#compileandRun()
+						#parseOutput(i,coreAppIndex)
+						i += 1
+
+				#print_to_csv(apps,coreList,values)
 
 
 #	if any("prim" in s for s in appList):
 #		addPrim(coreNums[appList.index("prim")])
 
+def parseOutput(outputType,coreAppIndex):
+	readFile = open(outputreadfile,'r')
+	outputs = []
+
+	for line in readFile:
+		outputs.append(line.split()[1])
+
+	readFile.close()
+
+	if outputType == 0:
+		values[0:4] = outputs[1:5]
+	elif outputType == 1:
+		values[4+coreAppIndex] = outputs[0]
+	elif outputType == 2:
+		values[4+len(applications)+coreAppIndex] = outputs[0]
+
+
+def compileandRun():
+	os.system('./build3')
+	sleep(10)
+	p1 = Popen(["python", "run.py", "prim"], stdout=PIPE, stdin=PIPE, shell=True, preexec_fn=os.setsid)
+	sleep(5)
+	os.killpg(p1.pid,signal.SIGTERM)
 
 def main():
 		# dummy example
 	global applications = ["prim"]
 	global chanMax = NUMCORES_INIT
-	cores = ["4 8 12"]
-	values = ["50","100","200","300","200","100","400","500"]	
-	
+	global values = []	
 
-	numCores = [4,4]
+	numCores = [4]
 
-	print_to_csv(applications,cores,values)
+	#print_to_csv(applications,cores,values)
 
 
 #	for x in (8,12):
 
 #		editFile(x)
-
-
 	runExperiments(applications,numCores)
-
 
 # 	add blur
 
